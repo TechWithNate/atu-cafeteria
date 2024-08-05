f4beed5186108fc3bf92e2c863df2d352139eb4b
NathanDzreke-Poku <dzrekenathan2002@gmail.com>
2024-08-05 14:30:28 +0000

    update

--- app/src/main/java/com/nate/atucafeteria/fragments/OrderFragment.java
+++ app/src/main/java/com/nate/atucafeteria/fragments/OrderFragment.java
@@ -1,6 +1,25 @@
 package com.nate.atucafeteria.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.nate.atucafeteria.R;
+
 public class OrderFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.order_fragment, container, false);
+
+        return view;
+    }
 }
--- app/src/main/java/com/nate/atucafeteria/fragments/SearchFragment.java
+++ app/src/main/java/com/nate/atucafeteria/fragments/SearchFragment.java
@@ -1,6 +1,24 @@
 package com.nate.atucafeteria.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.nate.atucafeteria.R;
+
 public class SearchFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.search_fragment, container, false);
+
+        return view;
+    }
 }
