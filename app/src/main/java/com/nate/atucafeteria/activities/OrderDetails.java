7992c849c6e9395e0b9f8db6b0c875e09e3b3c7f
NathanDzreke-Poku <dzrekenathan2002@gmail.com>
2024-08-09 18:03:52 +0000

    update

--- app/src/main/java/com/nate/atucafeteria/activities/OrderDetails.java
+++ app/src/main/java/com/nate/atucafeteria/activities/OrderDetails.java
@@ -1,4 +1,4 @@
-package com.nate.atucafeteria;
+package com.nate.atucafeteria.activities;
 
 import android.os.Bundle;
 
@@ -7,21 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import java.util.Scanner;
-public class MainActivity extends AppCompatActivity {
+
+import com.nate.atucafeteria.R;
+
+public class OrderDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_order_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
-
 }
